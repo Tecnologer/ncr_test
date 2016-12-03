0ae678111e64a6ed663e2b142ac1cb4176f1b915
Rey David Dominguez Soto <rdominguez@tecnologer.net>
2016-12-03 14:25:18 +0300

    Improvements UI and performance

--- js/utilities.js
+++ js/utilities.js
@@ -1,18 +1,24 @@
 function Utilities(input) {
-    input = input.split("\n")
+    var matchesIndex = [];
+    var matches = {};
+    var notValid = [];
+
+    var items = input.split("\n")
         .map(function(val) {
+            //remove spaces
             val = val.replace(/,\s*/g, ",");
             return val.split(",");
         });
 
     setsAreEquals = function(set1, set2) {
-        // if the other array is a falsy value, return
+        // if the other array is a false value, return
         if (!set2)
             return false;
 
         // compare lengths - can save a lot of time 
         if (set1.length != set2.length)
             return false;
+
         set1 = set1.map(Number).sort();
         set2 = set2.map(Number).sort();
         for (var i = 0, l = set1.length; i < l; i++) {
@@ -24,73 +30,66 @@ function Utilities(input) {
         return true;
     };
 
-    setIsValid = function(set1) {
-        for (var i = 0, l = set1.length; i < l; i++) {
-            if (isNaN(set1[i]) || set1[i] === "" || set1[i] == undefined)
+    setIsValid = function(set) {
+        for (var i = 0; i < set.length; i++) {
+            if (isNaN(set[i]) || set[i] === "" || set[i] == undefined)
                 return false;
         }
         return true;
     };
 
+    addDuplicated = function(pivot, duplicate) {
+        if (matches[pivot] !== null && typeof matches[pivot] !== 'object')
+                matches[pivot] = {duplicates: [], nonDuplicates: []};
+
+        matches[pivot].duplicates.push(duplicate);
+    };
+
+    addNonDuplicated = function(pivot, duplicate) {
+        if (matches[pivot] !== null && typeof matches[pivot] !== 'object')
+                matches[pivot] = {duplicates: [], nonDuplicates: []};
+
+        matches[pivot].nonDuplicates.push(duplicate);
+    };
+
+    addNotValid = function(set){
+        var nv = set.toString();
+        if (notValid.indexOf(nv) == -1) 
+            notValid.push(nv);
+    }
+
     return {
         getReportInformation: function() {
-            var items = input;
             var i = 0,
                 len = items.length;
-            var matchesIndex = [];
-            var matches = {};
-            var notValid = [];
+
             for (i = 0; i < len; i++) {
-                if (matchesIndex.indexOf(i) == -1 && notValid.indexOf(items[i]) == -1) {
+                if (matchesIndex.indexOf(i) == -1 && notValid.indexOf(items[i].toString()) == -1) {
                     if (setIsValid(items[i])) {
                         for (var j = i + 1; j < len; j++) {
                             if (setIsValid(items[j])) {
                                 if (setsAreEquals(items[i], items[j])) {
                                     matchesIndex.push(j);
-                                    if (matches["" + items[i]] !== null && typeof matches["" + items[i]] === 'object' && Array.isArray(matches["" + items[i]].d)) {
-                                        matches["" + items[i]].d.push(items[j]);
-                                    } else {
-                                        if (matches["" + items[i]] !== null && typeof matches["" + items[i]] !== 'object')
-                                            matches["" + items[i]] = {};
-
-                                        matches["" + items[i]]["d"] = [items[j]];
-                                    }
+                                    addDuplicated(items[i], items[j]);
                                 } else {
-                                    if (matches["" + items[i]] !== null && typeof matches["" + items[i]] === 'object' && Array.isArray(matches["" + items[i]].nd)) {
-                                        matches["" + items[i]].nd.push(items[j]);
-                                    } else {
-                                        if (matches["" + items[i]] !== null && typeof matches["" + items[i]] !== 'object')
-                                            matches["" + items[i]] = {};
-
-                                        matches["" + items[i]]["nd"] = [items[j]];
-                                    }
+                                    addNonDuplicated(items[i], items[j]);
                                 }
                             }
-                            //not valid
-                            else if (notValid.indexOf(items[j]) == -1) {
-                                notValid.push(items[j]);
-                            }
+                        }
 
+                        if (i > 0){
+                            for (var n = i - 1; n >= 0; n--) {
+                                if (setIsValid(items[n])) {
+                                    addNonDuplicated(items[i], items[n]);
+                                }
+                            }
                         }
                     }
                     //no valid
-                    else if (notValid.indexOf(items[i]) == -1) {
-                        notValid.push(items[i]);
+                    else {
+                        addNotValid(items[i]);
                     }
 
-                    if (i > 0)
-                        for (var n = i - 1; n >= 0; n--) {
-                            if (setIsValid(items[n])) {
-                                if (matches["" + items[i]] !== null && typeof matches["" + items[i]] === 'object' && Array.isArray(matches["" + items[i]].nd)) {
-                                    matches["" + items[i]].nd.push(items[n]);
-                                } else {
-                                    if (matches["" + items[i]] !== null && typeof matches["" + items[i]] !== 'object')
-                                        matches["" + items[i]] = {};
-
-                                    matches["" + items[i]]["nd"] = [items[n]];
-                                }
-                            }
-                        }
                 } /**/
 
             }
